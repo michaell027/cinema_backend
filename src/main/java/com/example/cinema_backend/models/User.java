7c7b8e34d815f716daf06e49da48ecc2182fa078
michaell027 <miselka12345@gmail.com>
2024-01-28 21:25:10 +0100

    default role user

--- src/main/java/com/example/cinema_backend/models/User.java
+++ src/main/java/com/example/cinema_backend/models/User.java
@@ -9,7 +9,6 @@ import java.util.Map;
 @Getter
 @Setter
 @NoArgsConstructor
-@RequiredArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "users")
@@ -19,25 +18,21 @@ public class User {
     @Column(name="id")
     private long id;
 
-    @NonNull
     @Column(name="email")
     private String email;
 
-    @NonNull
     @Column(name="password")
     private String password;
 
-    @NonNull
     @Column(name="first_name")
     private String firstName;
 
-    @NonNull
     @Column(name="last_name")
     private String lastName;
 
     @Column(name="role")
     @Enumerated(EnumType.STRING)
-    private Role role;
+    private Role role = Role.USER;
 
     public User (String email, String password) {
         this.email = email;
